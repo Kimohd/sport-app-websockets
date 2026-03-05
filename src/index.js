55b6663b1baf5b1dc073f766e377c9128137d00b
Kamal Hadini <kamalhd@MacBook-Pro-de-Kamal.local>
2026-03-05 00:27:39 +0000

    implement pub sub broadcast for commentaries

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {matchRouter} from "./routes/matches.js";
 import http from 'http';
 import { attachWebsocketServer } from './ws/server.js';
 import { securityMiddleware } from './arcjet.js';
+import { commentaryRouter } from './routes/commentary.js';
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || "0.0.0.0";
@@ -22,9 +23,11 @@ app.get('/', (req, res) =>{
 
 
 app.use('/matches', matchRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
 
-const { broadcastMatchCreated} = attachWebsocketServer(server);
+const { broadcastMatchCreated, broadcastCommentary} = attachWebsocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated; 
+app.locals.broadcastCommentary = broadcastCommentary; 
 
 // The Logging to let us know we're live
 server.listen(PORT, HOST,()=>{
